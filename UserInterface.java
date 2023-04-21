dd1e1e3c076684b9e52d0828c1c33392ec7699a4
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2023-04-21 13:07:57 -0400

    Packages and Swing basics

--- UserInterface.java
+++ UserInterface.java
@@ -1,2 +1,10 @@
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
 public class UserInterface {
+
+    private JFrame frame = new JFrame();
+    private JButton queryButtonOne = new JButton(), queryButtonTwo = new JButton(), queryButtonThree = new JButton(), queryButtonFour = new JButton();
+
 }
